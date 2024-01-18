ba26d58c89795a89e61a6478ea9f7067c46dcc1b
GlitchFake <92233778+GlitchFake@users.noreply.github.com>
2024-01-18 18:13:54 +0300

    a

--- MenuItems.cs
+++ MenuItems.cs
@@ -28,6 +28,38 @@ namespace QuBit.HueBox
         {
             PaintFolder.SetIconName("Blue");
         }
+        class EditorGUIColorField : EditorWindow
+        {
+            Color matColor = Color.white;
+            [MenuItem("Assets/HueBox/Set Color/Custom", false, menuItemPriority + 10)]
+            static void OpenColorPicker()
+            {
+                // EditorGUIColorField sýnýfýndan bir pencere oluþturun
+                var window = GetWindow<EditorGUIColorField>();
+
+                // Pencerenin pozisyonunu ve boyutunu belirtin
+                window.position = new Rect(500, 400, 170, 60);
+
+                // Pencereyi gösterin
+                window.Show();
+            }
+            void OnGUI()
+            {
+                // Renk seçimi için bir EditorGUI.ColorField oluþturun
+                matColor = EditorGUI.ColorField(new Rect(3, 3, position.width - 6, 15),
+                    "New Color:", // Renk seçim alanýnýn etiketi
+                    matColor);    // Varsayýlan renk deðeri
+
+                // "Change!" adýnda bir düðme oluþturun
+                if (GUI.Button(new Rect(3, 25, position.width - 6, 30), "Change!"))
+                {
+                    Debug.Log("Button Clicked!");
+                    Debug.Log("Selected Color: " + matColor.ToString());
+                    PaintFolder.SetFolderColor(matColor);
+                }
+            }
+
+        }
 
         [MenuItem("Assets/HueBox/Custom Color", false, menuItemPriority + 10)]
         static void CustomColor()
--- PaintFolder.cs
+++ PaintFolder.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -11,11 +11,14 @@ namespace QuBit.HueBox
     [InitializeOnLoad]
     static class PaintFolder
     {
+        private static Material folderMaterial;
+        const string FolderColorKey = "_Color";
         static PaintFolder()
         {
             EditorApplication.projectWindowItemOnGUI -= OnGUI;
             EditorApplication.projectWindowItemOnGUI += OnGUI;
         }
+
         private static void OnGUI(string guid, Rect selectionRect)
         {
             Color bgColor;
@@ -34,6 +37,7 @@ namespace QuBit.HueBox
                 AssetDatabase.LoadAssetAtPath<Texture2D>(folderTexturePath);
             GUI.DrawTexture(folderRect, folderTexture);
         }
+
 
         private static Rect GetFolderRect(Rect selectionRect, out Color backgroundColor)
         {
@@ -63,11 +67,90 @@ namespace QuBit.HueBox
             string folderPath = AssetDatabase.GetAssetPath(Selection.activeObject);
             string folderGuid = AssetDatabase.GUIDFromAssetPath(folderPath).ToString();
 
-            string iconPath = "Assets/HueBox/Icons/" + m_iconName + ".png";
+            string iconPath = "Assets/Editor/HueBox/Icons/" + m_iconName + ".png";
             string iconGuid = AssetDatabase.GUIDFromAssetPath(iconPath).ToString();
 
             EditorPrefs.SetString(folderGuid, iconGuid);
         }
+
+        public static void SetFolderColor(Color folderColor)
+        {
+            // Aktif nesnenin bir klasör olup olmadýðýný kontrol et
+            if (Selection.activeObject != null && AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(Selection.activeObject)))
+            {
+                // Klasör yolunu ve GUID'ini al
+                string folderPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+                string folderGuid = AssetDatabase.GUIDFromAssetPath(folderPath).ToString();
+
+                // Renk bilgisini kaydet
+                EditorPrefs.SetString(folderGuid + FolderColorKey, ColorUtility.ToHtmlStringRGBA(folderColor));
+                
+                // Klasör rengini uygula
+                ApplyColorToFolder(folderPath, folderColor);
+            }
+            else
+            {
+                Debug.LogWarning("Please select a valid folder.");
+            }
+        }
+
+        private static void ApplyColorToFolder(string folderPath, Color folderColor)
+        {
+            // Klasör altýndaki dosyalarý iþlemek yerine, sadece klasör rengini deðiþtir
+            Texture2D folderTexture = new Texture2D(16, 16);
+
+            // Tüm pikselleri istediðiniz renge ayarlayýn
+            Color[] pixels = new Color[folderTexture.width * folderTexture.height];
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = folderColor;
+            }
+
+            folderTexture.SetPixels(pixels);
+            folderTexture.Apply();
+
+            // Klasör için GUID alýnýr
+            string folderGuid = AssetDatabase.AssetPathToGUID(folderPath);
+
+            // Klasör için bir icon oluþturup EditorPrefs ile kaydedin
+            string folderIconPath = "Assets/Editor/HueBox/FolderIcons/" + folderGuid + "_FolderIcon.png";
+            File.WriteAllBytes(folderIconPath, folderTexture.EncodeToPNG());
+
+            // Oluþturulan ikonun GUID'sini alýn
+            string iconGuid = AssetDatabase.AssetPathToGUID(folderIconPath);
+
+            // Texture yükleme iþlemi baþarýsýz olduysa (dosya bulunamadý), uyarý ver ve iþlemi sonlandýr
+            if (string.IsNullOrEmpty(iconGuid))
+            {
+                Debug.LogError("Icon GUID is null or empty.");
+                return;
+            }
+
+            EditorGUIUtility.SetIconSize(new Vector2(16, 16));
+
+            // EditorGUIUtility.SetIconForObject metodu için bir Texture2D nesnesine ihtiyaç var
+            // Bu nedenle, yukarýda oluþturulan folderTexture'ý kullanarak bir nesne yaratýlmalýdýr
+            Texture2D folderTextureLoaded = new Texture2D(1, 1);
+            folderTextureLoaded.LoadImage(File.ReadAllBytes(folderIconPath));
+
+            // Texture yükleme iþlemi baþarýsýz olduysa (dosya bulunamadý), uyarý ver ve iþlemi sonlandýr
+            if (folderTextureLoaded == null)
+            {
+                Debug.LogError("Texture is null for GUID: " + iconGuid);
+                return;
+            }
+
+            EditorPrefs.SetString(folderGuid, folderIconPath);
+
+            // Klasör için simgeyi ayarla
+            EditorGUIUtility.SetIconForObject(
+            AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(folderPath),
+            AssetDatabase.LoadAssetAtPath<Texture2D>(folderIconPath)
+            );
+            EditorGUIUtility.SetIconSize(Vector2.zero);
+        }
+
+
 
         public static void ResetFolderTexture()
         {
@@ -77,8 +160,6 @@ namespace QuBit.HueBox
             EditorPrefs.DeleteKey(folderGuid);
             AssetDatabase.Refresh();
         }
-
-
     }
 }
 #endif
